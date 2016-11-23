d87aad5c97ecd93cf93de24658d6b8e4ade82f56
Victor Macias <v.maciascastillo@gmail.com>
2016-11-23 15:41:13 +0100

    refractor code

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,4 +1,5 @@
 class RestaurantsController < ApplicationController
+	before_action :set_restaurant, only: [:show]
 
 	def index
 		@restaurants = Restaurant.all
@@ -19,13 +20,16 @@ class RestaurantsController < ApplicationController
 	end
 
 	def show 
-		@restaurant = Restaurant.find(params[:id])
 	end
 
 	private
 
 	def restaurant_params
 		params.require(:restaurant).permit(:name, :address, :phone_number, :category)
+	end
+
+	def set_restaurant
+		@restaurant = Restaurant.find(params[:id])
 	end
 
 end
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,13 +1,12 @@
 class ReviewsController < ApplicationController
+  before_action :set_restaurant
+
 	def new 
-		@restaurant = Restaurant.find(params[:restaurant_id])
 		@review = Review.new
 	end
 
 	def create 
-		@restaurant = Restaurant.find(params[:restaurant_id])
 		@review = @restaurant.reviews.build(review_params)
-
 
 		if @review.save
 			redirect_to restaurant_path(@restaurant)
@@ -20,5 +19,9 @@ class ReviewsController < ApplicationController
 
 	def review_params
 		params.require(:review).permit(:content, :rating)
+	end
+
+	def set_restaurant
+		@restaurant = Restaurant.find(params[:restaurant_id])
 	end
 end
