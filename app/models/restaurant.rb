6b22deb9584e20d9c6fc274ed56c897ab5a3c219
Victor Macias <v.maciascastillo@gmail.com>
2016-11-23 12:36:11 +0100

    model

---
+++ app/models/restaurant.rb
@@ -0,0 +1,10 @@
+class Restaurant < ApplicationRecord
+	has_many :reviews, dependent: :destroy
+
+	validates :name, presence: true, uniqueness: true
+	validates :address, presence: true
+	validates :phone_number, presence: true
+	validates :category, inclusion: { in: %w(chinese italian japanese french belgian),
+		message: "%{value} is not a valid ctegory" }
+
+	end
